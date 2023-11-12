2a90514e255095ae2dde3170900fed4f6771fbeb
Chau Nguyen Truong An <anchau03102003@gmail.com>
2023-11-12 12:13:24 +0700

    Add metho Show for Cho.cs

---
+++ Cho.cs
@@ -0,0 +1,15 @@
+using System;
+class Man{
+    string Name;
+    int Age;
+    public Man(){
+        Name = "";
+        Age = "";
+    }    
+
+    public Man(string Name, int Age){
+        this.Name = Name;
+        this.Age = Age;
+    }
+}
+
